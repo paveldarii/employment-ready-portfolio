11ccdf3331706cfc5fc667716124403c8a950b6c
Pavel Darii <paveldarii@yahoo.com>
2020-12-08 23:24:32 -0500

    removed some code that was not making the program working

--- forms/contact.php
+++ forms/contact.php
@@ -6,7 +6,7 @@
   * For more info and help: https://bootstrapmade.com/php-email-form/
   */
 
-  // Replace contact@example.com with your real receiving email address
+  //Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'paveldarii@yahoo.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
@@ -25,12 +25,12 @@
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
+  // $contact->smtp = array(
+  //   'host' => 'mail.yahoo.com',
+  //   'username' => 'paveldarii@yahoo.com',
+  //   'password' => 'pass',
+  //   'port' => '587'
+  // );
   
 
   $contact->add_message( $_POST['name'], 'From');
@@ -38,4 +38,17 @@
   $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
-?>
+  // $name = $_POST["name"];
+  // $visitor_email= $_POST["email"];
+  // $message = $_POST["message"];
+  // $email_from = "paveldarii@yahoo.com";
+  // $email_subject = "New Form Submission";
+  // $email_body = "User Name: $name. \n".
+  //                 "User Email: $visitor_email. \n". 
+  //                    "User Message: $message. \n";
+  //   $to = "pavel96darii@mail.ru";
+  //   $headers = "From: $email_from \r\n";
+  //   $headers .= "Reply-To: $visitor_email \r\n";
+  //   mail($to, $email_subject, $email_body, $headers);
+  //   header("Location: index.html");
+  //   ?>
