c558f375b5846f1551008a48ac19f4a67b17caf0
Pavel Darii <paveldarii@yahoo.com>
2021-02-13 16:39:31 -0500

    in the server file are handled all data regarding starting the server and making communications between different files

---
+++ server.js
@@ -0,0 +1,32 @@
+// Requiring necessary npm packages
+const express = require("express");
+
+// Setting up port and requiring models for syncing
+const PORT = process.env.PORT || 8081;
+const db = require("./models");
+
+// Creating express app and configuring middleware needed for authentication
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//HandleBars Things
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Requiring our routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+});
