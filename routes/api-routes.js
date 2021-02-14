e8f624746019234645fd8577e699325048db8b3d
Pavel Darii <paveldarii@yahoo.com>
2021-02-13 23:23:22 -0500

    created the connection to the database to uploade data when user sends me an message

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,25 +2,36 @@ const db = require("../models");
 const mailgun = require("mailgun-js");
 module.exports = function (app) {
   app.post("/api/send_email", (req, res) => {
-    emailContent = `Name: ${req.body.name}
-Email: ${req.body.email}
-Text: ${req.body.text}`;
-    const data = {
-      from: process.env.mailgun_from_address,
-      to: "paveldarii@yahoo.com",
+    //save data in the db
+    db.Form.create({
+      name: req.body.name,
+      email: req.body.email,
       subject: req.body.subject,
-      text: emailContent,
-    };
-    console.log(data);
-    const DOMAIN = process.env.mailgun_domain;
-    console.log(DOMAIN);
-    const mg = mailgun({
-      apiKey: process.env.mailgun_api_key,
-      domain: DOMAIN,
+      text: req.body.text,
+    }).then((data) => {
+      res.json(data);
     });
 
-    mg.messages().send(data, function (error, body) {
-      res.json(body);
-    });
+    //     //sending email
+    //     emailContent = `Name: ${req.body.name}
+    // Email: ${req.body.email}
+    // Text: ${req.body.text}`;
+    //     const data = {
+    //       from: process.env.mailgun_from_address,
+    //       to: "paveldarii@yahoo.com",
+    //       subject: req.body.subject,
+    //       text: emailContent,
+    //     };
+    //     console.log(data);
+    //     const DOMAIN = process.env.mailgun_domain;
+    //     console.log(DOMAIN);
+    //     const mg = mailgun({
+    //       apiKey: process.env.mailgun_api_key,
+    //       domain: DOMAIN,
+    //     });
+
+    //     mg.messages().send(data, function (error, body) {
+    //       res.json(body);
+    //     });
   });
 };
