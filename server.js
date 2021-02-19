1532dcfb11e8b8316e34f40d499a2b97c13b5830
Pavel Darii <paveldarii@yahoo.com>
2021-02-18 20:15:32 -0500

    little change

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ var compression = require("compression");
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
-app.use(compression());
+
 // Creating express app and configuring middleware needed for authentication
 const app = express();
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
